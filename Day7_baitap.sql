3442ac657d6bdbdcc6bb256e4c5f4dd28221d4a1
amyynguyen <161178404+amyynguyen@users.noreply.github.com>
2024-03-04 17:49:11 -0700

    Update Day7_baitap.sql

--- Day7_baitap.sql
+++ Day7_baitap.sql
@@ -23,8 +23,8 @@ product_id,
 ROUND(AVG(stars), 2) AS avg_star
 FROM reviews
 
-GROUP BY mth, product_id
-ORDER mth, product_id
+GROUP BY EXTRACT(month FROM submit_date), product_id
+ORDER BY mth, product_id
 
 ---ex5: teams-power-users.
 SELECT sender_id,
@@ -49,4 +49,23 @@ WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date
 
 ---ex8: number-of-hires-during-specific-time-period.
+SELECT 
+COUNT(id) AS number_employeed
+FROM employees
+WHERE extract(month FROM joining_date) between 1 AND 7
+AND extract(year FROM joining_date) = 2022
+
+---ex9: positions-of-letter-a.
+SELECT
+position('a'in first_name) as position
+FROM worker
+WHERE first_name = 'Amitah'
+
+---ex10: macedonian-vintages.
+SELECT
+title,
+SUBSTRING (title, length(winery) + 2,4) as year
+FROM winemag_p2
+WHERE country = 'Macedonia'
+
 
