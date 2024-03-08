29c8e577d863d13425aa482c206a1fe7d0df7e87
amyynguyen <161178404+amyynguyen@users.noreply.github.com>
2024-03-07 19:07:28 -0700

    Update Day9_baitap.sql

--- Day9_baitap.sql
+++ Day9_baitap.sql
@@ -32,3 +32,11 @@ SELECT
 name
 FROM Customer
 WHERE COALESCE(referee_id, 0) != 2
+
+---ex5: stratascratch the-number-of-survivors.
+SELECT survived,
+SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+FROM titanic
+GROUP BY survived
