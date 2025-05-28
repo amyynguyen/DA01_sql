be1d8ae9da587c9fd1fe6636bcd6f4ae352934ee
amyynguyen <161178404+amyynguyen@users.noreply.github.com>
2025-05-28 14:01:58 -0600

    Update Day11.sql

--- Day11.sql
+++ Day11.sql
@@ -7,14 +7,25 @@ INNER JOIN COUNTRY
 ON CITY.COUNTRYCODE = COUNTRY.CODE
 GROUP BY COUNTRY.CONTINENT
 #ex2: datalemur-signup-confirmation-rate.
+SELECT 
+  ROUND(
+    COUNT(DISTINCT texts.email_id) * 1.0 / COUNT(DISTINCT emails.email_id), 
+    2
+  ) AS confirm_rate
+FROM emails 
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id AND texts.signup_action = 'Confirmed';
+  
 #ex3: datalemur-time-spent-snaps.
-  SELECT * FROM activities LIMIT 5;
-SELECT age.age_bucket, 
-  ROUND(SUM(activities.time_spent)*100 FILTER (WHERE activities.activity_type = 'send') / SUM(activities.time_spent), 2) AS send_perc,
-  ROUND(SUM(activities.time_spent)*100 FILTER (WHERE activities.activity_type = 'open') / SUM(activities.time_spent), 2) AS open_perc
+SELECT 
+  age_breakdown.age_bucket,
+  ROUND(SUM(CASE WHEN activities.activity_type = 'send' THEN activities.time_spent ELSE 0 END) * 100.0 / 
+        SUM(CASE WHEN activities.activity_type IN ('send', 'open') THEN activities.time_spent ELSE 0 END), 2) AS send_perc,
+  ROUND(SUM(CASE WHEN activities.activity_type = 'open' THEN activities.time_spent ELSE 0 END) * 100.0 / 
+        SUM(CASE WHEN activities.activity_type IN ('send', 'open') THEN activities.time_spent ELSE 0 END), 2) AS open_perc
 FROM activities
-
-INNER JOIN age_breakdown AS age
-ON activities.user_id = age.user_id
+JOIN age_breakdown
+  ON activities.user_id = age_breakdown.user_id
 WHERE activities.activity_type IN ('send', 'open')
-GROUP BY age.age_bucket
+GROUP BY age_breakdown.age_bucket;
+
