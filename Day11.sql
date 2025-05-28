e4fd3801043fc789cbf1fa60ff00f85d12774595
amyynguyen <161178404+amyynguyen@users.noreply.github.com>
2025-05-28 09:27:19 -0600

    Update Day11.sql

--- Day11.sql
+++ Day11.sql
@@ -1,4 +1,14 @@
-SELECT * FROM activities LIMIT 5;
+#ex1: hackerrank-average-population-of-each-continent.
+SELECT 
+COUNTRY.CONTINENT, 
+FLOOR(AVG(CITY.POPULATION))
+FROM CITY
+INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT
+#ex2: datalemur-signup-confirmation-rate.
+#ex3: datalemur-time-spent-snaps.
+  SELECT * FROM activities LIMIT 5;
 SELECT age.age_bucket, 
   ROUND(SUM(activities.time_spent)*100 FILTER (WHERE activities.activity_type = 'send') / SUM(activities.time_spent), 2) AS send_perc,
   ROUND(SUM(activities.time_spent)*100 FILTER (WHERE activities.activity_type = 'open') / SUM(activities.time_spent), 2) AS open_perc
